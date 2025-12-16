12a27437ed4bb79497a39c404134332be2f471f7
Ersin OZTURK <ersin.ozturk10@gmail.com>
2025-12-16 23:04:32 +0100

    bug_fix after ruff chec

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ def main():
 
     def dummy_function():
         print("This is a dummy function.")
-    
+
     def dummy_function_with_params(param1, param2):
         return f"Received parameters: {param1} and {param2}"
-    
+
     result = dummy_function_with_params("value1", "value2")
     print(result)
 
