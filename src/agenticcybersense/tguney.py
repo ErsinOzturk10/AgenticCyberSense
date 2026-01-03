402db7bab7643b7a80ede34db5156b76e668b89a
Tayyip Güney <84155765+tguneyt@users.noreply.github.com>
2026-01-03 19:15:14 +0100

    tguney test

--- src/agenticcybersense/tguney.py
+++ src/agenticcybersense/tguney.py
@@ -1,11 +1,13 @@
-"""This is a dummy test file for the tguney module in the agenticcybers"""
+"""Dummy test file for the tguney module in agenticcybersense."""
 
 
 def hello() -> None:
-    """Print a hello message."""
-    print("Hello World this is the newest test message from tguney")
+    """Hello message."""
+
+
+def goodby() -> None:
+    """Goodbye message."""
 
 
 hello()
-print("This is the end of the tguney test file.")
-"""end of tguney test file"""
+goodby()
