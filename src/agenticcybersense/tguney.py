3434ec7858a3fc9d87597b33298007d096424e31
Tayyip Güney <84155765+tguneyt@users.noreply.github.com>
2026-01-03 18:57:18 +0100

    tguney setup test

---
+++ src/agenticcybersense/tguney.py
@@ -0,0 +1,16 @@
+# tguneyt test file
+"""This is a dummy test file for the tguney module in the agenticcybers"""
+
+
+def hello() -> None:
+    """Print a hello message."""
+    print("Hello World this is the newest test message from tguney")
+
+
+hello()
+
+
+print("This is the end of the tguney test file.")
+
+# End of file
+"""end of tguney test file"""
